c0d7c2624c8fac4b9507084e4e1562d763ee8242
RudiPangestu <rudipangsentu@gmail.com>
2024-12-08 00:06:19 +0700

    tes

---
+++ src/Pages.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Footer from './component/Footer'
+import Header from './component/Header'
+import Home from './component/Home'
+import NavBar from './component/NavBar'
+
+
+
+
+
+function Pages() {
+  return (
+    <div>
+        <NavBar />
+        <Header />
+        <Home />
+        <Footer />
+    </div>
+  )
+}
+
+export default Pages
---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import Footer from './component/Footer.jsx'
+import NavBar from './component/NavBar.jsx'
+import Header from './component/Header.jsx'
+import Home from './component/Home.jsx'
+
+
+function main() {
+  return (
+    <div>
+      <NavBar/>
+      <Header/>
+      <Home/>
+      <Footer/>
+
+    </div>
+  )
+}
+
+export default main
